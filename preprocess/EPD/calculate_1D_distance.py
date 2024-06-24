5fca5d7993fcad41d44058dd146fd3992f41b859
HyunchangOh <ohyunchang@gmail.com>
2024-06-24 15:55:53 +0200

    implement calculate 1D distance for boolean column i.e. promoter with direction

--- preprocess/EPD/calculate_1D_distance.py
+++ preprocess/EPD/calculate_1D_distance.py
@@ -1,26 +1,32 @@
-def closest_true_distances(arr):
+# arr = a boolean array
+# direction = will the distance only be calculated from one direction? or both direction?
+# check with the example code below if you are unsure.
+
+def closest_true_distances(arr,direction="both"):
     n = len(arr)
     result = [float('inf')] * n
 
     # Traverse from left to right
-    closest_true_index = -1
-    for i in range(n):
-        if arr[i]:
-            closest_true_index = i
-        if closest_true_index != -1:
-            result[i] = i - closest_true_index
+    if direction == "both" or direction == "forward":
+        closest_true_index = -1
+        for i in range(n):
+            if arr[i]:
+                closest_true_index = i
+            if closest_true_index != -1:
+                result[i] = i - closest_true_index
 
     # Traverse from right to left
-    closest_true_index = -1
-    for i in range(n - 1, -1, -1):
-        if arr[i]:
-            closest_true_index = i
-        if closest_true_index != -1:
-            result[i] = min(result[i], closest_true_index - i)
+    if direction == "both" or direction =="reverse":
+        closest_true_index = -1
+        for i in range(n - 1, -1, -1):
+            if arr[i]:
+                closest_true_index = i
+            if closest_true_index != -1:
+                result[i] = min(result[i], closest_true_index - i)
     
     return result
 
 # Example usage:
-# input_list = [False, False, True, True, False, False, True, False]
-# output_list = closest_true_distances(input_list)
-# print(output_list)
+input_list = [False, False, True, True, False, False, True, False]
+output_list = closest_true_distances(input_list,"forward")
+print(output_list)
