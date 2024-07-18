aac19979be966c8843a18e025e10eb954ffb757a
HyunchangOh <ohyunchang@gmail.com>
2024-07-19 01:42:04 +0200

    model clean error and results

--- sandbox.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-input_dir = '/scratch/ohh98/LGT_subsampled_reboot/chr1/BIN50_CTCF.npy'
-
-legacy_dir =  '/scratch/ohh98/la_grande_table_subsampled/subsampled_data/chr1/BIN50_CTCF.npy'
-
-a = np.load(input_dir)
-b = np.load(legacy_dir)
-print(np.unique(b))
